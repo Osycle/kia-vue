31b15b13a2f789a522816b4f5b7f2a4bf30c7dee
Джанаев Азат <osycle@gmail.com>
2020-08-19 17:10:26 +0500

    19.08.2020

---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="bnr-carousel">
+      <div class="bnr-carousel-items owl-carousel owl-btn-2">
+        <figure>
+          <div class="container-p">
+            <div class="desc-content">
+              <div class="box-xs-10 box-md-9 m-auto">
+                <div class="wrapper">
+                  <h5>ВСТРЕЧАЙТЕ</h5>
+                  <h1>Новый KIA K5</h1>
+                </div>
+                <div class="carimgs">
+                  <img src="https://www.kia.ru/static/resize/1440x720/media-data/banners/main_header/k5/K5_side.png">
+                </div>
+                <div class="info-content">
+                  <p>До 30 июня 2020 года выгода 10% по госпрограммам льготного автокредитования</p>
+                  <div class="btn-content">
+                    <span class="btn-def">
+                      <a href="">Подробнее</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="img-content">
+            <div class="bg-shadow"></div>
+            <video src="https://www.kia.ru/static/media-data/banners/main_header/k5/K5-mainpage-2.mp4" muted="muted" autoplay="autoplay" loop="loop" preload="" playsinline=""></video>
+          </div>
+        </figure>
+        <figure>
+          <div class="container-p">
+            <div class="desc-content">
+              <div class="box-xs-10 box-md-9 m-auto">
+                <div class="wrapper">
+                  <h5>Sportage</h5>
+                  <h1>Направление - свобода</h1>
+                </div>
+                <div class="carimgs">
+                  <img src="https://www.kia.ru/static/resize/1440x720/media-data/banners/main_header/sportage/1920_Sportage_main_hero_model.png">
+                </div>
+                <div class="info-content">
+                  <p>Мультимедиа 10,25” с навигацией и камерой заднего вида • Электрообогрев лобового стекла, рулевого колеса и сидений • Drive Wise – комплекс систем безопасности и помощи водителю</p>
+                  <div class="btn-content">
+                    <span class="btn-def">
+                      <a href="">Подробнее</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="img-content">
+            <div class="bg-shadow"></div>
+            <video src="https://www.kia.ru/static/media-data/banners/main_header/sportage/1920_main_hero_banner.mp4" muted="muted" autoplay="autoplay" loop="loop" preload="" playsinline=""></video>
+          </div>
+        </figure>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
