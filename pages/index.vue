17fed8f8b0bb7b60396b390c25b37be6a408dd31
Джанаев Азат <osycle@gmail.com>
2020-10-08 17:26:03 +0500

    08.10.2020

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,41 @@
 <template>
   <div>
-    <Bnr />
+    <div class="bnr-carousel">
+      <div class="bnr-carousel-items owl-carousel owl-btn-2">
+
+        <figure v-for="(model, key) in pageData.content.banners" :key="key">
+          <div class="container-p">
+            <div class="desc-content">
+              <div class="box-xs-10 box-md-9 m-auto">
+                <div class="wrapper">
+                  <h5>{{model.title}}</h5>
+                  <h1>{{model.name}}</h1>
+                </div>
+                <div class="carimgs">
+                  <picture>
+                    <source :data-srcset="model.images.mobile">
+                    <img :src="model.images.desktop" sizes="272px">
+                  </picture>
+                </div>
+                <div class="info-content">
+                  <p>{{model.description}}</p>
+                  <div class="btn-content">
+                    <span class="btn-def">
+                      <a href="/k5">Подробнее</a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="img-content">
+            <div class="bg-shadow"></div>
+            <video :src="model.video" muted="muted" autoplay="autoplay" loop="loop" preload="" playsinline=""></video>
+          </div>
+        </figure>
+        
+      </div>
+    </div>
     <div class="short-models">
       <div class="block-wrapper p-v-md">
         <div class="container">
@@ -11,7 +46,7 @@
         <div class="container">
           <div class="short-models-nav m-v-20 text-center">
             <ul class="list flex-adaptive justify-c-center li-m-v-15">
-              <li v-for="(model, key) in models" :key="key" :class="{active: key == 0}">
+              <li v-for="(model, key) in pageData.content.model_list_info.model_lines" :key="key" :class="{active: key == 0}">
                 <a :href="'#smodels-'+[key+1]" data-toggle="tab">{{model.name}}</a>
               </li>
             </ul>
@@ -20,16 +55,53 @@
         <div class="short-models-content">
           <div class="tab-content">
 
-            <div :id="'smodels-'+[key+1]" v-for="(model, key) in models" :key="key" :class="{active: key == 0, 'tab-pane in': true}">
+            <div :id="'smodels-'+[key+1]" v-for="(model, key) in pageData.content.model_list_info.models" :key="key" :class="{active: key == 0, 'tab-pane in': true}">
               <div class="tab-content imgs-main">
-                <div class="img-content" :style="'background-image: url('+model.bg+');'">
+                <div class="img-content" :style="'background-image: url('+model.bg_images.desktop+');'">
                   <a href="/k5">
-                    <img :src="model.img">
+                    <img :src="model.image">
                   </a>
                 </div>
               </div>
             </div>
 
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="short-news">
+      <div class="container-p p-v-md">
+        <div class="entry-header m-t-30 text-center">
+          <h2>Будьте в курсе</h2>
+        </div>
+        <div class="short-models-nav m-v-20 text-center">
+          <ul class="list flex-adaptive justify-c-center li-m-v-15">
+            <li v-for="(groupName, key) in pageData.content.video_bank.groups" :class="{active: key == 0}" :key="key">
+              <a :href="'#short-news-'+[key]" data-toggle="tab">{{groupName.name}}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="tab-content m-v-30">
+          <div class="tab-pane fade in active" :id="'short-news-'+[key]" v-for="(groupName, key) in pageData.content.video_bank.groups" :key="key" :class="{active: key == 0, 'tab-pane in': true}">
+            <div class="short-news-items boxes-4 owl-carousel owl-btn-2">
+              <figure v-for="(overview, key) in pageData.content.video_bank.list" :key="key">
+                <div class="fig-wrapper">
+                  <div class="img-content">
+                    <a :href="[overview.video_link]" data-fancybox>
+                      <div class="btn-play">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M16 10l-9 5.196V4.804L16 10z" fill="currentColor"></path></svg>
+                      </div>
+                      <div class="img" :style="'background-image: url('+[overview.preview_image]+');'"></div>
+                    </a>
+                  </div>
+                  <div class="desc-content">
+                    <h4>{{overview.name}}</h4>
+                    <p>{{overview.author}}</p>
+                    <p class="news-date">{{ new Date(overview.date*1000) | dateFormat('D MMMM YYYY')}}</p>
+                  </div>
+                </div>
+              </figure>
+            </div>
           </div>
         </div>
       </div>
@@ -53,29 +125,30 @@
 
 <script>
 
-import Bnr from '@/components/Bnr'
+
 
 export default {
   components:{
-    Bnr,
+    
   },
   methods: {},
   computed: {},
   async asyncData({store, error}){
     try{
-      const models = await store.dispatch("models/fetchModels", {path: "index"})
-      console.log(JSON.parse(models));
-      //return {models}
+      const pageData = await store.dispatch("models/fetchModels", {path: "index"})
+      //console.log(pageData);
+      return {pageData}
     }catch(e){
       error(e);
     }
   },
   async created(){  
-    
+    //console.log(pageData);
   },
   data(){
     return {
       pageTitle: "Главная",
+      pageData:{},
       models:[],
       errors: [],
     }
@@ -90,9 +163,64 @@ export default {
   components: {
     
   },
-
-
-  
+  mounted(){
+		/*Owl carousel*/
+		window.owlBtn = [
+			'<span class="owl-btn previous">'+
+				'<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="">'+
+					'<path d="M6 3L2 7l4 4M2.333 7h12.334" stroke="currentColor" stroke-width="1.5"></path>'+
+				'</svg>'+
+			'</span>', 
+			'<span class="owl-btn next">'+
+				'<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="">'+
+    			'<path d="M9 11l4-4-4-4M12.667 7H.333" stroke="currentColor" stroke-width="1.5"></path>'+
+				'</svg>'+
+			'</span>'
+		]
+		$(".bnr-carousel-items.owl-carousel").owlCarousel({
+			nav: true,
+			loop: true,
+			//items: 3,
+			dots: true,
+			dotsEach: false,
+			//autoplay: true,
+			//autoplayTimeout: 7000,
+			autoheight: true,
+			touchDrag: true,
+			mouseDrag: true,
+			//smartSpeed: 0,
+			responsive:{
+				0:{items:1},
+				991:{items:1}
+			},
+			navText : owlBtn,
+			margin: 0
+    });
+		$(".short-news .short-news-items.owl-carousel").owlCarousel({
+			nav: !checkSm(),
+			loop: false,
+			//items: 3,
+			dots: false,
+			dotsEach: false,
+			slideBy: 2,
+			autoplay: false,
+			autoplayTimeout: 5400,
+			touchDrag: checkSm(),
+			//center: true,
+			responsive:{
+				0:{items:1},
+				991:{items:4}
+			},
+			navText : owlBtn,
+			margin: 30
+		});
+  },
+  filters: {
+    dateFilter(date){
+      date = new Date(date*1000)
+      return date.getFullYear()
+    }
+  }
 }
 
 </script>
--- store/index.js
+++ store/index.js
@@ -1,52 +1,88 @@
 
 import Vue from 'vue'
-import Vuex from 'vuex'
 
+import Vuex from 'vuex'
 Vue.use(Vuex)
 
-
+import VueFilterDateFormat from '@vuejs-community/vue-filter-date-format';
+Vue.use(VueFilterDateFormat, {
+  dayOfWeekNames: [
+    'Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг',
+    'Пятница', 'Суббота'
+  ],
+  dayOfWeekNamesShort: [
+    'Su', 'Mo', 'Tu', 'We', 'Tr', 'Fr', 'Sa'
+  ],
+  monthNames: [
+    'Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'
+  ],
+  monthNamesShort: [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+  ],
+  // Timezone offset, in minutes (0 - UTC, 180 - Russia, undefined - current)
+  timezone: 5
+});
 
 import ApiPlugin from '@/plugins/api'
 Vue.use(ApiPlugin);
 
 
 
-// import reactions from "../data/models.js"
 
-// const loadModelsIndex = (time) => {
-//   return new Promise((resolve) => {
-//     setTimeout(()=>{
-//       resolve(reactions)
-//     }, 1000)
-//   })
-// }
+		
+// COMMON FUNCTION
 
-// const store = () =>
-//   new Vuex.Store({
-//     state: {
-//       models: []
-//     },
-//     getters: {
-//       getModels(state){
-//         return state.models
-//       }
-//     },
-//     mutations: {
-//       SET_REACTIONS(state, payload){
-//         state.models = payload
-//       }
-//     },
-//     actions: {
-//       async loadModelsIndex({ commit }, payload){
-//         try {
-//           const reactions = await loadModelsIndex(payload)
-//           commit('SET_REACTIONS', reactions)
-//           //console.log(modelsIndex);
-//         }catch(error){
-//           console.error(error);
-//         }
-//       }
-//     }
-//   })
 
-// export default store
+function checkSm() {
+	return $(document).width() <= 991;
+}
+
+function checkMd() {
+	return $(document).width() < 1199 && !checkSm();
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntFloat(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function onResized(f) {
+	if (typeof f === "function") f();
+	$(window).on("resize", function(e) {
+		if (typeof f === "function") f();
+	});
+	return this;
+}
+
+function scrolledDiv(el) {
+	try {
+		var docViewTop = $(window).scrollTop(),
+			docViewBottom = docViewTop + $(window).height(),
+			elTop = $(el).offset().top + 100,
+			elBottom = elTop + $(el).height() / 1.8;
+	} catch (err) {
+		console.error();
+	}
+
+	return elBottom <= docViewBottom && elTop >= docViewTop;
+}
+
+function roundFix( num, cnt ){
+	num = num+""
+	cnt = cnt + (/./.test(num) || null ? 1 : 0);
+	return num.substring( 0,  cnt)*1
+}
+
+
+function spaceBetweenNum(str, char) {
+	str = str+"";
+	char = char || ","
+	var pattern = /(-?\d+)(\d{3})/;
+	while (pattern.test(str))
+		str = str.replace(pattern, "$1"+char+"$2");
+	return str;
+}
