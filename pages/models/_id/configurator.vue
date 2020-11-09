717fa2078c62cda8b80b8f659b0b1f3090274fe3
Джанаев Азат <osycle@gmail.com>
2020-11-09 17:21:11 +0500

    09.11.2020

---
+++ pages/models/_id/configurator.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <Breadcrump :breadcrumpItems="breadcrumpItems"/>
+  </div>
+</template>
+
+<script>
+
+
+import mainjs from '@/static/js/main'
+
+export default {
+  head() {
+    return {
+      title: this.page.seo.title,
+      meta: [
+        {
+          content: this.page.seo.description
+        }
+      ],
+    }
+  },
+  async asyncData(context){
+    try{
+      const path = context.route.path
+      const page = await context.store.dispatch("models/fetchPageData", {
+        path
+      })
+      return {page: page.content}
+    }catch(e){
+      context.error(e);
+    }
+  },
+  data(){
+    return {
+      //
+    }
+  },
+  mounted(){
+    mainjs();
+  },
+  methods: {}
+}
+
+</script>
