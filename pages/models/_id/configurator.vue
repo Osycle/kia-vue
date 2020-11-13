3c983ddf7cb14cd5a949532b4fcfdff02f681995
Джанаев Азат <osycle@gmail.com>
2020-11-13 16:49:33 +0500

    13.11.2020

--- pages/models/_id/configurator.vue
+++ pages/models/_id/configurator.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="relative">
+  <div class="relative conf" scrollf>
     <Breadcrump :breadcrumpItems="breadcrumpItems"/>
-    <div class="conf" scrollf>
+    <div class="conf-header">
       <div class="container-p">
         <div class="entry-header">
           <h2>Конфигуратор</h2>
         </div>
-        <div class="conf-progress-bar m-v-30">
+        <div class="conf-progress-bar">
           <ul class="list">
             <li class="active">
               <b class="color-1">01</b>
@@ -30,7 +30,11 @@
             </li>
           </ul>
         </div>
-        <div class="conf-steps conf-step-1">
+      </div>
+    </div>
+    <div class="conf-content">
+      <div class="container-p">
+        <div class="conf-steps conf-step-1 hide">
           <div class="conf-crs m-v-45">
             <div class="owl-carousel owl-btn-2">
               <template v-for="(modelLine) in page.model_lines">
@@ -67,14 +71,101 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="conf-down">
-        <div class="container-p">
-          <div class="flex-wrapper">
-            <span class="btn-def">
-              <a href="javascript:;" currentstep="0" @click="confnext">Далее</a>
-            </span>
+        <div class="conf-steps conf-step-2">
+          <div class="row p-b-10">
+            <div class="left-bar-def col-md-3">
+              <div class="wrapper conf-result-content">
+                <div class="cap-content">
+                  <h3>K5</h3>
+                  <h3>от 3 504 900 ₽</h3>
+                  <h3>17 570 ₽/мес</h3>
+                </div>
+                <figure>
+                  <img src="https://cdn.kia.ru/resize/300x200/master-data/models/image_side/stinger.png" alt="">
+                </figure>
+                <div class="conf-result-section">
+                  <dl>
+                    <dt>2020 год производства</dt>
+                  </dl>
+                </div>
+                <br>
+                <div class="conf-result-section">
+                  <h4>Двигатель и трансмиссия</h4>
+                  <dl><dt>3.3 V6 T-GDI, 370 л.с., бензин</dt></dl> 
+                  <dl><dt>8AT, автомат</dt></dl> 
+                  <dl><dt>4WD, полный</dt></dl>
+                </div>
+                <div class="conf-result-summary">
+                  <dl>
+                    <dt>Итоговая стоимость</dt> 
+                    <dd><strong class="text-s1">от 3 504 900 ₽</strong></dd>
+                  </dl>
+                </div>
+                <div class="conf-result-summary">
+                  <p>Кредитный расчет</p>
+                  <dl>
+                    <dt>Ежемесячный платеж</dt> 
+                    <dd><strong class="text-s1">10 900 ₽/мес</strong></dd>
+                  </dl>
+                </div>
+              </div>
+            </div>
+            <div class="entry-content col-md-9">
+              <div class="car-params">
+                <div class="car-params-item">
+                  <h4>Двигатель</h4>
+                  <ul class="flex-list">
+                    <li>
+                      <div class="car-params-btn active">
+                        <div class="flex">
+                          <figure class="check-sel"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M10 5v10M5 10h10" stroke="currentColor" stroke-width="2"></path></svg></figure>
+                          <div class="m-l-15"><div><strong>2.0 MPI</strong></div>150 л.c., бензин</div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="car-params-btn">
+                        <div class="flex">
+                          <figure class="check-sel"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M10 5v10M5 10h10" stroke="currentColor" stroke-width="2"></path></svg></figure>
+                          <div class="m-l-15"><div><strong>2.0 MPI</strong></div>150 л.c., бензин</div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="car-params-item">
+                  <h4>Двигатель</h4>
+                  <ul class="flex-list">
+                    <li>
+                      <div class="car-params-btn active">
+                        <div class="flex">
+                          <figure class="check-sel"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M10 5v10M5 10h10" stroke="currentColor" stroke-width="2"></path></svg></figure>
+                          <div class="m-l-15"><div><strong>2.0 MPI</strong></div>150 л.c., бензин</div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="car-params-btn">
+                        <div class="flex">
+                          <figure class="check-sel"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M10 5v10M5 10h10" stroke="currentColor" stroke-width="2"></path></svg></figure>
+                          <div class="m-l-15"><div><strong>2.0 MPI</strong></div>150 л.c., бензин</div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="conf-down">
+      <div class="container-p">
+        <div class="flex-wrapper">
+          <span class="btn-def">
+            <a href="javascript:;" currentstep="0" @click="confnext">Далее</a>
+          </span>
         </div>
       </div>
     </div>
