9182e7db8b26ff5170cb35a20fbc5fcec54aca76
Джанаев Азат <osycle@gmail.com>
2021-01-26 16:55:30 +0500

    26.01.2021

--- store/models.js
+++ store/models.js
@@ -2,8 +2,8 @@
 export const actions = {
   async fetchPageData(context, params){
     try{
-      const models = await this.$axios.$get('http://kia-api-php/handler.php', {params})
-      //const models = await this.$axios.$get('http://html.lifestyle.uz/kia-api/handler.php', {params})
+      //const models = await this.$axios.$get('http://kia-api-php/handler.php', {params})
+      const models = await this.$axios.$get('http://html.lifestyle.uz/kia-api/handler.php', {params})
       return models
     }catch(e){
       throw e
@@ -11,8 +11,8 @@ export const actions = {
   },
   async fetchVideo(context, params){
     try{
-      const modelVideo = await this.$axios.$get('http://kia-api-php/video_bank.php', {params})
-      //const modelVideo = await this.$axios.$get('http://html.lifestyle.uz/kia-api/video_bank.php', {params})
+      //const modelVideo = await this.$axios.$get('http://kia-api-php/video_bank.php', {params})
+      const modelVideo = await this.$axios.$get('http://html.lifestyle.uz/kia-api/video_bank.php', {params})
       return modelVideo
     }catch(e){
       throw e
