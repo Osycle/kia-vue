2cc18f4e597eb68dcce9fe865699b8c25bdc2e71
Джанаев Азат <osycle@gmail.com>
2021-01-29 16:38:13 +0500

    20210129

--- pages/models/_id/cars.vue
+++ pages/models/_id/cars.vue
@@ -1,37 +1,6 @@
 <template>
-  <div class="main-body offset-model-header">
-    <div class="model-header header-scroll scrolled-down">
-      <div class="container-p">
-        <div class="model-header-panel">
-          <div class="align-center">
-            <span class="name-content">{{page.model.name}}</span>
-            <span class="price-content">от {{page.model.min_price | spaceBetweenNum}} сум</span>
-            <a class="flex m-l-15 hidden-xs hidden-sm" href="javascript:;" data-src="#textcredit" data-fancybox><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="none" role="button" class="color-gray-3 info-icon-gray info-icon"><circle cx="10" cy="10" r="9.25" stroke="currentColor" stroke-width="1.5"></circle><path d="M9 15h2V8.5H9V15z" fill="currentColor"></path><circle cx="10" cy="6.25" r="1.25" fill="currentColor"></circle></svg></a>
-            <a href=".list-add-sub" class="btn-options visible-xs visible-sm" data-toggle="dropdown"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class=""><path d="M5 8l5 5 5-5" stroke="currentColor" stroke-width="2"></path></svg></a>
-          </div>
-          <div class="model-header-nav">
-            <ul class="list list-main">
-              <li v-for="(item, key) in page.model_menu" :key="key"><nuxt-link active-class="active" :to="item.link">{{item.title}}</nuxt-link></li>
-            </ul>
-            <div class="list-add">
-              <a href=".list-add-sub" class="btn-options hidden-xs hidden-sm" data-toggle="dropdown">
-                <div class="icm-area"><svg width="18" height="4" viewBox="0 0 18 4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><circle cx="2" cy="2" r="2"></circle><circle cx="9" cy="2" r="2"></circle><circle cx="16" cy="2" r="2"></circle></svg></div>
-              </a>
-              <div class="list-add-sub">
-                <ul>
-                  <li v-for="(item, key) in page.model_menu" :key="key"><nuxt-link active-class="active" :to="item.link">{{item.title}}</nuxt-link></li>
-                </ul>
-              </div>
-            </div>
-            <div class="list-last">
-							<nuxt-link :to="'/models/'+page.model.code+'/configurator'">Конфигуратор</nuxt-link>
-            </div>
-          </div>
-
-        </div>
-      </div>
-    </div>
-    <div class="options-bnr relative card-top">
+  <div class="main-body carstock">
+    <div class="options-bnr relative">
       <div class="container-p relative">
         <div class="flex-adaptive align-center justify-c-between">
 					<div class="breadcrumb-container">
@@ -39,45 +8,161 @@
 							<li><nuxt-link to="/">Главная</nuxt-link></li>
 							<li><nuxt-link to="/models">Модели</nuxt-link></li>
 							<li><nuxt-link :to="'/models/'+page.model.code">{{page.model.name}}</nuxt-link></li>
-							<li><nuxt-link :to="'/models/'+page.model.code+'/properties'">Характеристики</nuxt-link></li>
+							<li><nuxt-link to="">Каталог новых автомобилей</nuxt-link></li>
 						</ol>
 					</div>
         </div>
-        <div class="entry-intro">
+        <div class="entry-intro m-b-md-50 m-v-30">
           <div class="relative">
-            <h2>Характеристики {{page.model.name}}</h2>
+            <h1>{{page.model.name}} в Ташкенте</h1>
+            <p>Новые автомобили в наличии у официальных дилеров KIA</p>
           </div>
         </div>
       </div>
     </div>
-		<div class="options-entry scrolled-down">
-			<div class="container-p relative">
-				<div class="config-sidebar">	
-					<div class="config-filter">
-						<div class="config-filter-items">
+    <div class="entry-content border-top">
+      <div class="options-entry scrolled-down">
+        <div class="container-p relative">
+          <div class="config-sidebar">	
+            <div class="config-filter">
+              <div class="config-filter-items">
+                <fieldset>
+                  <h4>Двигатель</h4>
+                  <div class="input-items">
+                    <label role="button" class="align-center m-v-15">
+                      <input type="checkbox" name="" class="none" >
+                      <span class="checkbox-style-1"></span>
+                      <span class="m-l-10">2.0 MPI / 149 л.с. / Бензин</span>
+                    </label>
+                    <label role="button" class="align-center m-v-15">
+                      <input type="checkbox" name="" class="none" >
+                      <span class="checkbox-style-1"></span>
+                      <span class="m-l-10">1.6 T-GDI / 177 л.с. / Бензин</span>
+                    </label>
+                  </div>
+                </fieldset>
 							<fieldset>
-								<h4>Двигатель</h4>
+								<h4>Комплектация</h4>
 								<div class="input-items">
 									<label role="button" class="align-center m-v-15">
 										<input type="checkbox" name="" class="none" >
 										<span class="checkbox-style-1"></span>
-										<span class="m-l-10">2.0 MPI / 149 л.с. / Бензин</span>
+										<span class="m-l-10">Classic</span>
 									</label>
 									<label role="button" class="align-center m-v-15">
 										<input type="checkbox" name="" class="none" >
 										<span class="checkbox-style-1"></span>
-										<span class="m-l-10">1.6 T-GDI / 177 л.с. / Бензин</span>
+										<span class="m-l-10">Comfort</span>
+									</label>
+									<label role="button" class="align-center m-v-15">
+										<input type="checkbox" name="" class="none" >
+										<span class="checkbox-style-1"></span>
+										<span class="m-l-10">Luxe</span>
+									</label>
+									<label role="button" class="align-center m-v-15">
+										<input type="checkbox" name="" class="none" >
+										<span class="checkbox-style-1"></span>
+										<span class="m-l-10">Prestige</span>
+									</label>
+									<label role="button" class="align-center m-v-15">
+										<input type="checkbox" name="" class="none" >
+										<span class="checkbox-style-1"></span>
+										<span class="m-l-10">Style</span>
+									</label>
+									<label role="button" class="align-center m-v-15">
+										<input type="checkbox" name="" class="none" >
+										<span class="checkbox-style-1"></span>
+										<span class="m-l-10">GT line</span>
 									</label>
 								</div>
 							</fieldset>
-						</div>
-					</div>
-				</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="carstock-content p-h-30 p-h-md-60">
+            <div class="carstock-items figure-m-v-30">
+              <div class="item">
+                <figure>
+                  <img src="https://cdn.kia.ru/resize/445x250/master-data/albums/k5/UD/1.png" alt="">
+                </figure>
+                <div class="desc-content">
+                  <h4><a href="" class="btn-link-1">K5 Classic</a></h4>
+                  <div class="param-content color-gray-5">
+                    <div class="m-v-5">2.0 л / 150 л.c. / бензин / автомат / передний привод</div>
+                    <div class="m-v-5">2020 год производства / 2021 модельный год</div>
+                    <div class="m-v-5">Цвет: Clear White</div>
+                    <div class="m-v-5">Салон: Черный, Тканевая отделка</div>
+                  </div>
+                  <div class="price-content m-t-20">
+                    <strong><big>1 589 900 сум</big></strong>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <figure>
+                  <img src="https://cdn.kia.ru/resize/445x250/master-data/albums/k5/UD/1.png" alt="">
+                </figure>
+                <div class="desc-content">
+                  <h4>
+                    <a href="" class="btn-link-1">K5 Classic</a>
+                  </h4>
+                  <div class="param-content color-gray-5">
+                    <div class="m-v-5">2.0 л / 150 л.c. / бензин / автомат / передний привод</div>
+                    <div class="m-v-5">2020 год производства / 2021 модельный год</div>
+                    <div class="m-v-5">Цвет: Clear White</div>
+                    <div class="m-v-5">Салон: Черный, Тканевая отделка</div>
+                  </div>
+                  <div class="price-content m-t-20">
+                    <strong><big>1 589 900 сум</big></strong>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <figure>
+                  <img src="https://cdn.kia.ru/resize/445x250/master-data/albums/k5/UD/1.png" alt="">
+                </figure>
+                <div class="desc-content">
+                  <h4>
+                    <a href="" class="btn-link-1">K5 Classic</a>
+                  </h4>
+                  <div class="param-content color-gray-5">
+                    <div class="m-v-5">2.0 л / 150 л.c. / бензин / автомат / передний привод</div>
+                    <div class="m-v-5">2020 год производства / 2021 модельный год</div>
+                    <div class="m-v-5">Цвет: Clear White</div>
+                    <div class="m-v-5">Салон: Черный, Тканевая отделка</div>
+                  </div>
+                  <div class="price-content m-t-20">
+                    <strong><big>1 589 900 сум</big></strong>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <figure>
+                  <img src="https://cdn.kia.ru/resize/445x250/master-data/albums/k5/UD/1.png" alt="">
+                </figure>
+                <div class="desc-content">
+                  <h4>
+                    <a href="" class="btn-link-1">K5 Classic</a>
+                  </h4>
+                  <div class="param-content color-gray-5">
+                    <div class="m-v-5">2.0 л / 150 л.c. / бензин / автомат / передний привод</div>
+                    <div class="m-v-5">2020 год производства / 2021 модельный год</div>
+                    <div class="m-v-5">Цвет: Clear White</div>
+                    <div class="m-v-5">Салон: Черный, Тканевая отделка</div>
+                  </div>
+                  <div class="price-content m-t-20">
+                    <strong><big>1 589 900 сум</big></strong>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
 
-
-			</div>
-		</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
