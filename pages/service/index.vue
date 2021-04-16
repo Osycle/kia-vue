de308c2d5d51e4d370454f71bfcf8d8d665d677c
Джанаев Азат <osycle@gmail.com>
2021-04-16 17:45:53 +0500

    20210416

---
+++ pages/service/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="main-body offset-header services">
+    <div class="container-p">
+      <div class="breadcrumb-container">
+        <ol class="breadcrumb">
+          <li><nuxt-link to="/">Главная</nuxt-link></li>
+          <li><nuxt-link to="">Владельцам</nuxt-link></li>
+        </ol>
+      </div>
+      <div class="entry-header m-v-30">
+        <h1 class="text-x5">Владельцам Kia</h1>
+        <p>Запишитесь на сервис, подберите аксессуары и запчасти не выходя из дома.</p>
+      </div>
+      <div class="services-items boxes-4 figure-m-v-15 mv-4 mb-6">
+        <div class="cell">
+          <a href="">
+            <div class="item-wrapper">
+              <div class="icon-content mb-4"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill-rule="evenodd" clip-rule="evenodd" d="M32 4H4v39h7v-2H6V6h24v4h2V4z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33 19H16v-2h17v2zM33 24H16v-2h17v2zM27 29H16v-2h11v2zM27 34H16v-2h11v2z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 12h25v13.678l2-2V10H10v37h29V32.163l-2 2V45H12V12z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M44.183 21.324l1.414 1.414-11.704 11.704-1.414-1.415 11.704-11.703zm0-2.829l4.242 4.243L33.894 37.27l-4.242-4.243 14.532-14.532zM33 38l-4-4v4h4zM27 39H16v-2h11v2z" fill="currentColor"></path></svg></div>
+              <div class="desc-content">
+                <span class="hover-aunderline mv-4 fw-6 text-s1">
+                  <span class="this-btn mb-3">Запись на сервис</span>
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="services-item__title-icon"><path d="M8.5 14l4-4-4-4" stroke="currentColor" stroke-width="2"></path></svg>
+                </span>
+                <div class="color-gray-6 mt-4">Запишитесь на сервис в удобное для вас время</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="cell">
+          <a href="">
+            <div class="item-wrapper">
+              <div class="icon-content mb-4"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill-rule="evenodd" clip-rule="evenodd" d="M32 4H4v39h7v-2H6V6h24v4h2V4z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33 19H16v-2h17v2zM33 24H16v-2h17v2zM27 29H16v-2h11v2zM27 34H16v-2h11v2z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 12h25v13.678l2-2V10H10v37h29V32.163l-2 2V45H12V12z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M44.183 21.324l1.414 1.414-11.704 11.704-1.414-1.415 11.704-11.703zm0-2.829l4.242 4.243L33.894 37.27l-4.242-4.243 14.532-14.532zM33 38l-4-4v4h4zM27 39H16v-2h11v2z" fill="currentColor"></path></svg></div>
+              <div class="desc-content">
+                <span class="hover-aunderline mv-4 fw-6 text-s1">
+                  <span class="this-btn mb-3">Запись на сервис</span>
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="services-item__title-icon"><path d="M8.5 14l4-4-4-4" stroke="currentColor" stroke-width="2"></path></svg>
+                </span>
+                <div class="color-gray-6 mt-4">Запишитесь на сервис в удобное для вас время</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="cell">
+          <a href="">
+            <div class="item-wrapper">
+              <div class="icon-content mb-4"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill-rule="evenodd" clip-rule="evenodd" d="M32 4H4v39h7v-2H6V6h24v4h2V4z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33 19H16v-2h17v2zM33 24H16v-2h17v2zM27 29H16v-2h11v2zM27 34H16v-2h11v2z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 12h25v13.678l2-2V10H10v37h29V32.163l-2 2V45H12V12z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M44.183 21.324l1.414 1.414-11.704 11.704-1.414-1.415 11.704-11.703zm0-2.829l4.242 4.243L33.894 37.27l-4.242-4.243 14.532-14.532zM33 38l-4-4v4h4zM27 39H16v-2h11v2z" fill="currentColor"></path></svg></div>
+              <div class="desc-content">
+                <span class="hover-aunderline mv-4 fw-6 text-s1">
+                  <span class="this-btn mb-3">Запись на сервис</span>
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="services-item__title-icon"><path d="M8.5 14l4-4-4-4" stroke="currentColor" stroke-width="2"></path></svg>
+                </span>
+                <div class="color-gray-6 mt-4">Запишитесь на сервис в удобное для вас время</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="cell">
+          <a href="">
+            <div class="item-wrapper">
+              <div class="icon-content mb-4"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill-rule="evenodd" clip-rule="evenodd" d="M32 4H4v39h7v-2H6V6h24v4h2V4z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33 19H16v-2h17v2zM33 24H16v-2h17v2zM27 29H16v-2h11v2zM27 34H16v-2h11v2z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 12h25v13.678l2-2V10H10v37h29V32.163l-2 2V45H12V12z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M44.183 21.324l1.414 1.414-11.704 11.704-1.414-1.415 11.704-11.703zm0-2.829l4.242 4.243L33.894 37.27l-4.242-4.243 14.532-14.532zM33 38l-4-4v4h4zM27 39H16v-2h11v2z" fill="currentColor"></path></svg></div>
+              <div class="desc-content">
+                <span class="hover-aunderline mv-4 fw-6 text-s1">
+                  <span class="this-btn mb-3">Запись на сервис</span>
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="services-item__title-icon"><path d="M8.5 14l4-4-4-4" stroke="currentColor" stroke-width="2"></path></svg>
+                </span>
+                <div class="color-gray-6 mt-4">Запишитесь на сервис в удобное для вас время</div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="cell">
+          <a href="">
+            <div class="item-wrapper">
+              <div class="icon-content mb-4"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid"><path fill-rule="evenodd" clip-rule="evenodd" d="M32 4H4v39h7v-2H6V6h24v4h2V4z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M33 19H16v-2h17v2zM33 24H16v-2h17v2zM27 29H16v-2h11v2zM27 34H16v-2h11v2z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 12h25v13.678l2-2V10H10v37h29V32.163l-2 2V45H12V12z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M44.183 21.324l1.414 1.414-11.704 11.704-1.414-1.415 11.704-11.703zm0-2.829l4.242 4.243L33.894 37.27l-4.242-4.243 14.532-14.532zM33 38l-4-4v4h4zM27 39H16v-2h11v2z" fill="currentColor"></path></svg></div>
+              <div class="desc-content">
+                <span class="hover-aunderline mv-4 fw-6 text-s1">
+                  <span class="this-btn mb-3">Запись на сервис</span>
+                  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="services-item__title-icon"><path d="M8.5 14l4-4-4-4" stroke="currentColor" stroke-width="2"></path></svg>
+                </span>
+                <div class="color-gray-6 mt-4">Запишитесь на сервис в удобное для вас время</div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
