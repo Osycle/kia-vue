632c842ded4d33d89d6c077f9a79374508af5a9e
Джанаев Азат <osycle@gmail.com>
2021-05-10 17:52:28 +0500

    20210510 добавлена новость

--- pages/models/_id/configurator2/index.vue
+++ pages/models/_id/configurator2/index.vue
@@ -156,29 +156,29 @@
               </div>
             </div>
           </template>
-           <template v-else-if="currentStepNum === 3">
+          <template v-else-if="currentStepNum === 3">
             <div class="conf-steps conf-step-3">
               <div class="entry-content">
                 <div class="entry-title m-v-30">
-                  <h3>{{model.title}} {{currentComplectation.name}}{{currentComplectation.bodyColors[0].colorFolder}}</h3>
+                  <h3>{{model.title}} {{currentComplectation.name}}</h3>
                 </div>
-                <div class="showroom-main m-v-10" v-if="false">
+                <div class="showroom-main m-v-10">
                   <div
                       class="cloudimage-360"
-                      :data-folder="currentComplectation.bodyColors[0].colorFolder"
+                      :data-folder="currentShowroom.colorFolder"
                       data-filename="{index}.png"
                       data-spin-reverse
                       data-amount="72">
                       <div class="showroom-item-cover flex align-center">
                         <div class="flex box-xs-10 align-center">
-                          <img :src="currentComplectation.bodyColors[0].colorFolder+'/1.png'" width="100%">
+                          <img :src="currentShowroom.colorFolder+'/1.png'" width="100%">
                         </div>
                       </div>
                   </div>
                 </div>
                 <script>window.CI360 = { notInitOnLoad: true }</script>
                 <script src="/js/plugins/js-cloudimage-360-view.min.js"></script>
-                <div v-if="false" class="color-gray-4 text-center m-b-30">
+                <div class="color-gray-4 text-center m-b-30">
                   <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" class="showroom__three-sixty-icon"><path d="M9.724 9.472c0-1.584-.912-2.28-2.82-2.28-.6 0-1.632.18-2.268.468v1.296c.816-.276 1.5-.396 2.268-.384.864.012 1.164.336 1.164.924 0 1.056-.948 1.284-1.752 1.284H5.68v1.392h.636c1.212 0 2.064.228 2.064 1.332-.036.816-.42 1.212-1.476 1.212-.768 0-1.344-.06-2.556-.348v1.308a8.962 8.962 0 002.7.444c1.848 0 2.988-.792 2.988-2.616 0-1.26-.552-1.896-1.416-2.196.672-.372 1.104-.96 1.104-1.836zM14.343 14.788c-1.488 0-1.584-1.2-1.584-2.988a5.527 5.527 0 011.572-.252c.972 0 1.38.384 1.38 1.548 0 1.116-.324 1.692-1.368 1.692zm.324-6.228c.612 0 1.308.108 2.124.312V7.6a7.966 7.966 0 00-2.436-.408c-1.668 0-3.264.744-3.264 4.764 0 2.628.912 4.176 3.264 4.176 2.064 0 3.012-1.272 3.012-3.156 0-1.776-.96-2.808-2.736-2.808-.528 0-1.176.096-1.836.3.132-1.548.924-1.908 1.872-1.908zM21.62 16.132c1.968 0 3.132-1.344 3.132-4.428 0-3.084-1.164-4.512-3.132-4.512-1.968 0-3.12 1.428-3.12 4.512 0 3.096 1.152 4.428 3.12 4.428zm0-1.344c-1.116 0-1.464-1.008-1.464-3.084 0-2.028.348-3.168 1.464-3.168s1.476 1.14 1.476 3.168c0 2.076-.36 3.084-1.476 3.084z" fill="currentColor"></path><path d="M15 23c-7.732 0-14-2.727-14-6.09 0-1.263.885-2.437 2.4-3.41m17.667 8.9C25.76 21.417 29 19.328 29 16.91c0-1.263-.885-2.437-2.4-3.41" stroke="currentColor" stroke-width="1.5"></path><circle cx="27.5" cy="6.5" r="1.75" stroke="currentColor" stroke-width="1.5"></circle><path d="M12 19l4 4-4 4" stroke="currentColor" stroke-width="1.5"></path></svg>
                   <p><small>Изображение может не соответствовать выбранной комплектации. Цвет автомобиля может отличаться от представленного на данном сайте.</small></p>
                 </div>
@@ -328,6 +328,7 @@ export default {
       currentPrice: 0,
       currentComplectation: {},
       currentComplectations: [],
+      currentShowroom: {},
 
       currentModel: {},
       currentModelLine: {},
@@ -353,7 +354,12 @@ export default {
     $('.sidebar-wrapper').theiaStickySidebar({
       additionalMarginTop: 0
     });
+    // Начальный выбор комплектации
     this.selectParamEngine(this.currentEngine);
+    // Активация showroom 360 при клике
+    $(document).on("click", ".showroom-item-cover", ()=>{
+      window.CI360.init();
+    })
   },
   methods: {
     validParams(val, arr){
@@ -419,6 +425,7 @@ export default {
 
         this.currentComplectations = await this.$axios.$get(url)
         this.currentComplectation = this.currentComplectations[0];
+        this.currentShowroom = this.currentComplectation.bodyColors[0];
         console.log(this.currentComplectations);
       }
 
