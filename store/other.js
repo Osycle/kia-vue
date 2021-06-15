01cd58cf762aaae81008623d803746405a749173
Osycle <osycle@gmail.com>
2021-06-15 11:14:00 +0500

    20210615 1113

--- store/other.js
+++ store/other.js
@@ -3,7 +3,9 @@ export const actions = {
   async fetchPath(context, params){
     try {
       console.log(params.path);
-      const data = await this.$axios.$get("https://api.kia-motors.uz/"+params.path);
+      if(params.path.substring(params.path.length-1) == "/")
+        params.path = params.path.substring(0, params.path.length-1)
+      const data = await this.$axios.$get("https://api.kia-motors.uz"+params.path);
       return data;
     } catch (e) {
       throw e;
@@ -15,6 +17,8 @@ export const actions = {
       console.log(params);
       if(!params.path)
          path = 0;
+      // if(params.path.substring(params.path.length-1) == "/")
+      //    params.path = params.path.substring(0, params.path.length-1)
       const model = await this.$axios.$get("https://api.kia-motors.uz/article/all/"+path);
       return model;
     } catch (e) {
